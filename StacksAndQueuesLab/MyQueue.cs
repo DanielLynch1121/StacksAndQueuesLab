b8e2f45d75b7abc4c4b2fa00a0712114776c6d84
Daniel Lynch <cplayma1@outlook.com>
2023-10-08 23:16:27 -0400

    Add project files.

---
+++ StacksAndQueuesLab/MyQueue.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueuesLab
+{
+    public class MyQueue<T> : IQueue<T>
+    {
+        private List<T> items = new List<T>();
+        public int Count => items.Count;
+
+        public T Dequeue()
+        {
+            T item = items[0];
+            items.RemoveAt(0);
+            return item;
+        }
+
+        public void Enqueue(T item)
+        {
+            items.Add(item);
+        }
+
+        public T Peek()
+        {
+            return items[0];
+        }
+    }
+}
